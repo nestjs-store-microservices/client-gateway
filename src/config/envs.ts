d09f305642b6ddf0a0b5a0b9c6e05072de98a04e
lgcholoquinga <luisg.choloquinga@gamil.com>
2025-01-30 20:59:50 -0500

    add url frontedns origin

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -11,18 +11,22 @@ interface EnvVars {
   PORT_ORDERS_MS: number;
 
   NATS_SERVERS: string[];
+
+  URL_FRONTENDS: string[];
 }
 
 const envsScheme = joi
   .object({
     PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
+    URL_FRONTENDS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
 const { error, value } = envsScheme.validate({
   ...process.env,
   NATS_SERVERS: process.env.NATS_SERVERS?.split(';'),
+  URL_FRONTENDS: process.env.URL_FRONTENDS?.split(';'),
 });
 
 if (error) {
@@ -34,4 +38,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   natsServers: envVars.NATS_SERVERS,
+  urlFrontEnds: envVars.URL_FRONTENDS,
 };
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,20 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 import { RpcCustomExceptionFilter } from './common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const looger = new Logger('Client-Gateway');
 
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: envs.urlFrontEnds,
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
